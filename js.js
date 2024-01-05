bfe7096d48b7112eea47120b9dfc6378cf63e955
Nicholas-Battista <battistan23@gmail.com>
2024-01-04 19:34:21 -0700

    Upadted js to change div color on click and drag
    updated the createGrid function to now check for a click
    or click and drag to color the divs the mouse crosses over
    
    created a handleMouseEvent function to know if the mouse is being clicked which is passed into the createGrid function

--- js.js
+++ js.js
@@ -1,12 +1,32 @@
 const container = document.querySelector('.main-container');
+let isMouseDown = false;
 
 
-function createGrid () {
-    for (let i = 0; i < 16 * 16; i++) {
+function handleMouseEvent(event) {
+    if (event.type === "mousedown"){
+        isMouseDown = true;
+    }
+    if (event.type === "mouseup"){
+        isMouseDown = false;
+    }
+    if (event.type === "mousedown" || (event.type === "mouseover" && isMouseDown)) {
+        this.style.backgroundColor = "blue";
+    }
+};
+
+
+function createGrid (LW) {
+    for (let i = 0; i < (LW * LW); i++) {
         let div = document.createElement("div");
         div.classList.add("grid-item");
+
+        div.addEventListener('mouseover', handleMouseEvent);
+        div.addEventListener('mousedown', handleMouseEvent);
+        div.addEventListener('mouseup', handleMouseEvent);
+
         container.appendChild(div);
     };
 };
 
-window.onload = createGrid();
+window.onload = createGrid(16);
+
